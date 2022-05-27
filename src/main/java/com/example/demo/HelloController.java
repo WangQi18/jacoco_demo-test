936d44c2881f56abcc9cb38184ceb6ecf5493d4b
WangQi18 <qi@bjtu.edu.cn>
2022-05-27 15:07:11 +0800

    new

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("hello")
+    public String say() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("byebye")
+    public String byebye() {
+        return "byebye!";
+    }
+
+    @RequestMapping("haaaha")
+    public String haaaha() {
+        return "haaaha!";
+    }
+
+
+}
